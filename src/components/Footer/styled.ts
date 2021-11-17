64d75db9a63255b0f442895e13932f55efe825ec
metfor <mateus_frade@hotmail.com>
2021-11-17 16:51:56 -0300

    fix: correções

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -1,40 +1,49 @@
 import styled from "styled-components";
-export const ContainerFooter=styled.div`
-width: 80%;
-height: 250px;
-display :flex;
-align-items :center;
-justify-content :center;
-margin-top:327px;
-`
-export const BarPurple =styled.div`
-width:3px;
-height:30px;
-background-color:#6610f2;
+export const ContainerFooter = styled.div`
+  width: 80%;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 277px;
+`;
+export const BarPurple = styled.div`
+  width: 3px;
+  height: 30px;
+  background-color: #6610f2;
+  margin-left: 200px;
+`;
+export const BoxComent = styled.div`
+  display: grid;
+  margin-left: 300px;
+  grid-template-columns:5px 200px;
+`;
+export const Name = styled.h1`
+  font-family: Sentence;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24.06px;
+  color: #2f2f2f;
+  opacity: 0.75;
+  margin-top: 1px;
+  margin-left: 10px;
+`;
+export const Coment = styled.p`
+  font-family: Inter;
+  font-weight: 400;
+  font-style: italic;
+  font-size: 16px;
+  line-height: 26px;
+`;
+export const BoxComent2 = styled.div`
+margin-left: -700px;
+display: grid;
+margin-top:120px;
+grid-template-columns: 360px 360px;
+justify-content:space-between;
 
 `
-export const BoxComent=styled.div`
-    display:grid;
-    margin-left:300px;
-    grid-template-columns: 5px 360px;
-    
-    
-
-`
-export const Name=styled.h1`
-    font-family:Sentence;
-    font-weight:400;
-    font-size:20px;
-    line-height:24.06px;
-    color:#2f2f2f;
-    opacity:0.75;
-    margin-top: 1px;
-    margin-left: 10px;
-`
-export const Coment=styled.p`
-    font-family:Inter;
-    font-weight:400;
-    font-style:italic;
-    font-size:16px;
-    line-height:26px;
-`
+export const Box = styled.div`
+display: grid;
+grid-template-columns: 170px;
+grid-template-rows:200px 20px;`
--- src/components/HeaderV/index.tsx
+++ src/components/HeaderV/index.tsx
@@ -1,19 +1,24 @@
-import { ButtonLogin, ContainerHeader, Logo, Search, SearchImage } from "./styles";
+import {
+  ButtonLogin,
+  ContainerHeader,
+  Logo,
+  Search,
+  SearchImage,
+} from "./styles";
 
-export function HeaderV(){
-    return(
-        <ContainerHeader>
-            <Logo>CodeMoji</Logo>
-            <Search>
-             <SearchImage
-                src="/../public/search.png"
-                alt="search"
-                width="16.67"
-                height="16.67"
-            />
-            </Search>
-            <ButtonLogin>Fazer Login</ButtonLogin>
-        </ContainerHeader>
-    )
-
+export function HeaderV() {
+  return (
+    <ContainerHeader>
+      <Logo>CodeMoji</Logo>
+      <Search>
+        <SearchImage
+          src="/../public/search.png"
+          alt="search"
+          width="16.67"
+          height="16.67"
+        />
+      </Search>
+      <ButtonLogin>Fazer Login</ButtonLogin>
+    </ContainerHeader>
+  );
 }
